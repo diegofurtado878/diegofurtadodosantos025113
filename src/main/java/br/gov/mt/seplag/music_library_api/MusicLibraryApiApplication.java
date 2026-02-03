e6356a7ebc97ba675844f91cc8576171804c35b3
Diego Furtado dos Santos <02511336138@seguranca.local>
2026-02-02 23:31:48 -0400

    Initial commit - base

---
+++ src/main/java/br/gov/mt/seplag/music_library_api/MusicLibraryApiApplication.java
@@ -0,0 +1,13 @@
+package br.gov.mt.seplag.music_library_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicLibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicLibraryApiApplication.class, args);
+	}
+
+}
